157b7bda635daecbc3eeda85818bff809832823b
Marc Matias <mrcallenmatias@gmail.com>
2022-04-05 14:37:49 -0700

    completed frontend structure

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import "./style.css"
+
+
+class Header extends Component {
+    render() {
+        return(
+            <>
+                <div id="header">
+                    <h1>Welcome to Crypto Tinder!!!</h1>
+                </div>
+                
+            </>
+        )
+    }
+}
+
+export default Header
---
+++ src/pages/CryptoEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class CryptoEdit extends Component {
+    render() {
+        return(
+            <>
+                <h1>CryptoEdit</h1>
+            </>
+        )
+    }
+}
+
+export default CryptoEdit
